97fd0d2c72f2f00f25b489a3285621aa324c1cd3
Douglas Fernandes Pereira Junior <douglas.fernandes91@live.com>
2022-10-21 14:43:30 -0300

    add filters

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import * as momentTimezone from 'moment-timezone';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './filters/http.exception.filter';
 
 async function bootstrap() {
+  const PORT = 8080;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  Date.prototype.toJSON = (): any => {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
+
+  await app.listen(process.env.PORT || PORT);
 }
 bootstrap();
